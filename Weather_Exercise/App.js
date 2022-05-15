274d97ab4bd5b2448ff6961f14295ac385ac9981
ayobna <ayobnas@gmail.com>
2022-05-16 01:22:30 +0300

    weatherapi

--- Weather_Exercise/App.js
+++ Weather_Exercise/App.js
@@ -1,20 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, ScrollView, Alert ,Text} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { _storeData } from "./utils/Functions";
+import { Provider as PaperProvider, Searchbar ,Card } from "react-native-paper";
+import axios from "axios";
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const [data, setData] = useState([])
+  const api =
+    "https://api.weatherapi.com/v1/current.json?key=0b9fe350d38644b0a4193829221505&q=";
+  const onChangeSearch =  (query) => {
+    setSearchQuery(query);
+    console.log(query);
+  };
+  const getWeather =   () => {console.log("999")
+
+    axios.get(api + `` + searchQuery).then((response) => {
+      ;
+      //console.log(data)
+      const tempData = [ ...data,response.data];
+      setData(tempData);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+        <Searchbar
+          placeholder="חיפוש"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+      
+          onIconPress={getWeather}
+         // onPressOut={getWeather}
+       //   onKeyPress={getWeather}
+        />
+        <View style={styles.container}>
+          <ScrollView
+    
+          >
+        
+            {
+           data.length>0&&  data.map((item, index) => {
+              {    console.log("999999999999999999999",item.location.country)}
+              return (
+                <Card key={index}>
+                  <Card.Title
+                    title={item.location.name}
+    
+                   subtitle={item.location.country}
+                   
+                  />
+                    <Card.Cover source={{ uri: 'https:'+ item.current.condition.icon }} />
+                </Card>
+                  )})
+                
+              }
+          </ScrollView>
+        </View>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
---
+++ Weather_Exercise/utils/Functions.js
@@ -0,0 +1,40 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const _getData = async (key) => {
+
+    try {
+      let d = await AsyncStorage.getItem(key)
+      if (d !== null) {
+        console.log('get Data Key=: ' + key)
+        return (JSON.parse(d))
+      }
+      console.log(key + ' is null')
+      return null;
+    }
+    catch (error) {
+      console.log(error)
+      return null;
+    }
+  }
+  
+  export const _storeData = async (Key, obj) => {
+    try {
+      await AsyncStorage.setItem(Key, JSON.stringify(obj));
+      console.log(`${Key} save successes`);
+      return true;
+    }
+    catch (error) {
+      console.log(error);
+      return null;
+    }
+  }
+  
+  export const _removeData = async (key) => {
+    try {
+      await AsyncStorage.removeItem(key);
+      return true;
+    } catch (e) {
+      console.log("error while remove data from async storage")
+    }
+  };
+  
